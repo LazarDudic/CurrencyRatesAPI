b2f08ad4c3f407452a2ff4be2b41499ab9aa4981
Lazar Dudic <lazar.dudic@yahoo.com>
2021-02-11 21:09:41 +0100

    Add Laravel Sanctum auth token

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'min:6', 'string']
+        ]);
+
+        if($validator->fails()){
+            return response($validator->messages(), 422);
+        }
+
+        $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+        ]);
+
+        return response($user, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response($validator->messages(), 422);
+        }
+
+        $credentials = $request->only('email', 'password');
+        if (!auth()->attempt($credentials)) {
+            return response()->json([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'password' => [
+                        'Invalid credentials'
+                    ],
+                ]
+            ], 422);
+        }
+
+        $user = User::where('email', $request->email)->first();
+        $authToken = $user->createToken('access:token')->plainTextToken;
+
+        return response(['access_token' => $authToken,], 200);
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [\App\Http\Controllers\Auth\AuthController::class, 'register'])->name('register');
+Route::post('/login', [\App\Http\Controllers\Auth\AuthController::class, 'login'])->name('login');
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/latest', [\App\Http\Controllers\LatestController::class, 'index']);
+    Route::get('/history/', [\App\Http\Controllers\HistoryController::class, 'index']);
+    Route::get('/history/{date}', [\App\Http\Controllers\HistoryController::class, 'show']);
 });
 
-Route::apiResource('/latest', \App\Http\Controllers\LatestController::class)
-    ->only('index');
-
-Route::get('/history/', [\App\Http\Controllers\HistoryController::class, 'index']);
-Route::get('/history/{date}', [\App\Http\Controllers\HistoryController::class, 'show']);
